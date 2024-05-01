fbe0ee7a2594126ede9d8591dd2774cf025a41bb
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-05-01 15:55:00 -0300

    converting types

---
+++ converting_types.py
@@ -0,0 +1,21 @@
+# float -> int
+print(int(1.0546878))
+
+# str -> int
+print(int("10"))
+
+# str -> float
+print(float("10.10"))
+
+# int -> float
+print(float(100))
+
+# float -> str
+value = 10
+value_str = str(value)
+print(type(value))
+print(type(value_str))
+
+# divisões
+print(100 / 2) # retorna float
+print(100 // 2) # retorna int
