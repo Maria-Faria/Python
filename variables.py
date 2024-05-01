aa575790aae6123c04369ce66dfef7add16015cc
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-05-01 15:40:03 -0300

    variables and constants

---
+++ variables.py
@@ -0,0 +1,17 @@
+"""age = 20
+name = 'Maria Eduarda'
+print(f'My name is {name} and I am {age} years old.')"""
+
+age, name = (20, 'Maria Eduarda')
+print(f'My name is {name} and I am {age} years old.')
+
+# alterando valores de uma variável
+age = 28
+name = 'Joaquim'
+print(f'My name is {name} and I am {age} years old.')
+
+daily_withdrawal_limit = 1000
+
+# constantes
+STATES = ['SP', 'RJ', 'MG', 'ES']
+print(STATES)
