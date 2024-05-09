c5d1193f49b682bd5cc6b625cc31a07a71dddb60
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-05-09 09:03:45 -0300

    operadores de comparação

---
+++ Tipos_Operadores/operadores_comparacao.py
@@ -0,0 +1,20 @@
+saldo = 200
+saque = 200
+
+# igualdade
+print(f'Saldo é igual ao saque? {saldo == saque}')
+
+# Diferença
+print(f'Saldo é diferente de saque? {saldo != saque}')
+
+# Maior que
+print(f'Saldo é maior que saque? {saldo > saque}')
+
+# Maior ou igual
+print(f'Saldo é maior ou igual a saque?{saldo >= saque}')
+
+# Menor que
+print(f'Saldo é menor que saque? {saldo < saque}')
+
+# Menor ou igual
+print(f'Saldo é menor ou igual a {saldo <= saque}')
