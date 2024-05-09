60eb5a72a11d5884a51b7311a4510aa8e5753982
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-05-09 09:14:44 -0300

    operadores de atribuição

---
+++ Tipos_Operadores/operadores_atribuicao.py
@@ -0,0 +1,36 @@
+saldo = 500
+print(saldo)
+
+print('\nSobrescrevendo o valor da variável...')
+
+# sobrescrever
+saldo = 200
+print(saldo)
+
+print('\nSomando...')
+saldo += 10
+print(saldo)
+
+print('\nSubtraindo...')
+saldo -= 5
+print(saldo)
+
+print('\nDivisão Inteira...') # só funciona com números inteiros
+saldo //= 2
+print(saldo)
+
+print('\nDividindo...')
+saldo /= 2
+print(saldo)
+
+print('\nMultiplicando...')
+saldo *= 10
+print(saldo)
+
+print('\nMódulo...')
+saldo %= 4
+print(saldo)
+
+print('\nExponenciando...')
+saldo **= 2
+print(saldo)
