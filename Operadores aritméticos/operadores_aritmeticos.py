5c910b691b3c2ac8cc68f22ba7b73b52e8b741fa
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-05-09 08:56:11 -0300

    operadores aritmeticos

---
+++ Operadores aritméticos/operadores_aritmeticos.py
@@ -0,0 +1,35 @@
+produto_1 = 20
+produto_2 = 10
+
+# soma
+print(produto_1 + produto_2)
+
+# subtração
+print(produto_1 - produto_2)
+
+# divisão
+print(produto_1 / produto_2)
+
+# divisão inteira
+print(produto_1 // produto_2)
+
+# multiplicação
+print(produto_1 * produto_2)
+
+# módulo
+print(produto_1 % produto_2)
+
+# exponenciação
+print(produto_1 ** produto_2)
+
+# expressões
+x = 10 + 5 * 4
+print(x)
+
+# precedente - parênteses
+x = (10 + 5) * 4
+print(x)
+
+# expoente > divisão > multiplicação > soma > subtração
+y = (10 / 2) + 25 * ((2 - 2) ** 2)
+print(y)
