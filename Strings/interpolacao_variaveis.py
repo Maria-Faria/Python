265c5646e4750ceea1ce4983a58e57cc463b693a
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-05-25 21:20:45 -0300

    interpolação de variáveis

---
+++ Strings/interpolacao_variaveis.py
@@ -0,0 +1,19 @@
+name = "Maria"
+age = 20
+profession = "Programadora"
+language = "Python"
+
+data = {"name": "Maria", "age": 20}
+
+# usando porcentagem
+print("Nome: %s Idade: %d" % (name, age))
+
+# método Format
+print("Nome: {} Idade: {}".format(name, age))
+print("Nome: {1} Idade: {0}".format(age, name))
+print("Nome: {1} Idade: {0} Nome: {1}".format(age, name))
+print("Nome: {name} Idade: {age}".format(age=age, name=name))
+print("Nome: {name} Idade: {age}".format(**data)) # usando um dicionário
+
+# f-string
+print(f"Nome: {name} Idade: {age}")
