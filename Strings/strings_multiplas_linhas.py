6e4bcf7052b26bb1d1cbbb8fe4ff3bad26aba802
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-05-25 21:41:54 -0300

    strings múltiplas linhas

---
+++ Strings/strings_multiplas_linhas.py
@@ -0,0 +1,19 @@
+name = "Maria"
+
+message = f"""
+Olá, meu nome é {name},
+Eu estou estudando Python
+"""
+
+print(message)
+
+print("""
+    ********** MENU **********
+      
+      1 - Depositar
+      2 - Sacar
+      3 - Sair
+
+    ***************************
+    Obrigado por usar nosso sistema!
+""")
