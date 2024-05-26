6384a52a99dfdd5f61eea17ea1faedb861a62b4e
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-05-25 21:34:51 -0300

    fatiamento de strings

---
+++ Strings/fatiamento_string.py
@@ -0,0 +1,28 @@
+name = "Maria Eduarda de Faria"
+
+#retornar um caractere
+print(name[0])
+
+#do início ao índice 5
+print(name[:5])
+
+#do índice 6 ao final
+print(name[6:])
+
+#definindo início e fim
+print(name[6:13])
+
+#início, fim e passo
+print(name[6:13:2])
+
+#sem parâmetros
+print(name[:])
+
+#exibindo ao contrário
+print(name[::-1])
+
+#exibir a última letra
+print(name[-1])
+
+#exibir a penúltima letra
+print(name[-2])
