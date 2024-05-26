d75740fc98f25b4e0b097e48db779e616fa9152c
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-05-26 14:45:04 -0300

    criação de listas

---
+++ Listas/criacao_listas.py
@@ -0,0 +1,13 @@
+#criando listas com colchetes
+fruits = ["laranja", "maçã", "uva"]
+print(fruits)
+
+fruits = []
+print(fruits)
+
+#criando listas com construtor list
+letters = list("python")
+print(letters)
+
+numbers = list(range(10))
+print(numbers)
